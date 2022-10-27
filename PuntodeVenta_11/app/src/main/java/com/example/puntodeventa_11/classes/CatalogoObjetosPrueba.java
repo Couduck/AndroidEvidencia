6589253beeca8da1112108c79c7eb54b21d4a44d
Patricio <patoestradac@gmail.com>
2022-10-27 05:28:59 -0500

    Parte de Adrian

---
+++ PuntodeVenta_11/app/src/main/java/com/example/puntodeventa_11/classes/CatalogoObjetosPrueba.java
@@ -0,0 +1,8 @@
+package com.example.puntodeventa_11.classes;
+
+public class CatalogoObjetosPrueba
+{
+    public String[] nombres = {"Producto 1", "Producto 2", "Producto 3"};
+    public String[] precios = {"$200", "$100", "$300"};
+    public String[] descripciones = {"Descripcion 1", "Descripcion 2", "Descripcion 3"};
+}
